068b48c5a6a8ed902e719ece35ae0b557de093b7
Tung Dao <tungdao17@gmail.com>
2022-04-02 00:23:46 +0800

    Fix redis db backup

--- build_sys.sh
+++ build_sys.sh
@@ -13,11 +13,14 @@ fi
 export DOCKER_IMG=aaqua_sys_img
 export GRPC_PORT=8000
 export REST_PORT=5000
-
-export DB_HOST_NAME=aaqua_db
 export DB_PORT=6379
 
 docker build -t $DOCKER_IMG -f Dockerfile .
 docker-compose build
-docker-compose --profile $TEST up --remove-orphans
-# docker-compose stop
+
+docker network create aaqua_net
+
+# Solve redis db mouting issue
+# https://github.com/docker/compose/issues/2481#issuecomment-808721577
+docker-compose down
+docker-compose --profile $TEST up --remove-orphans --force-recreate
